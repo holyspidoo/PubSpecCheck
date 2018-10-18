220eba3457786da57b6385de517d9d0a632b533c
JG <infojgmx@googlemail.com>
2018-10-18 12:47:31 -0400

    Added Changelog option flag

--- README.md
+++ README.md
@@ -25,6 +25,7 @@ HostedDependency: ^3.0.0 => 3.0.2 	 pub_client
 HostedDependency: ^1.5.0 => 1.5.0 	 args
 ```
 
+
 If a major change is detected, it will give you the link to the changelog to see if there are any breaking changes. Here is an example:
 
 ```
@@ -34,6 +35,42 @@ HostedDependency: 0.7.0 => 0.8.0         flutter_inapp_purchase
 ------------------------------------
 HostedDependency: ^0.5.18 => 0.5.20      firebase_auth
 ```
+
+
+Please note that if you don't use ^ in your yaml file, you will get a notification of a major difference as well.
+
+```
+--------- MAJOR DIFFERENCE ---------
+Changelog: https://pub.dartlang.org/packages/flutter_inapp_purchase#-changelog-tab-
+HostedDependency: 0.8.0 => 0.8.2         flutter_inapp_purchase
+------------------------------------
+```
+
+## Show all changelogs
+
+With the -c flag, all changelogs urls will be shown for packages where the versions are different from those found in the pubspec.yaml file.
+
+For example, when running this:
+```
+> dart SpecCheck.dart pubspec.yaml
+```
+
+You might get something like:
+```
+------------------------------------
+Changelog: https://pub.dartlang.org/packages/timeago#-changelog-tab-
+HostedDependency: ^2.0.1 => 2.0.8        timeago
+
+--------- MAJOR DIFFERENCE ---------
+Changelog: https://pub.dartlang.org/packages/flutter_inapp_purchase#-changelog-tab-
+HostedDependency: 0.8.0 => 0.8.2         flutter_inapp_purchase
+------------------------------------
+------------------------------------
+No new version, no changelog needed
+HostedDependency: ^0.3.2 => 0.3.2        package_info
+------------------------------------
+```
+
 
 ## Notes
 
--- SpecCheck.dart
+++ SpecCheck.dart
@@ -5,10 +5,18 @@ import 'package:pub_client/pub_client.dart';
 import 'package:pubspec_parse/pubspec_parse.dart' as pbs;
 import 'package:pub_semver/pub_semver.dart' as sem;
 
+const showChangeLogsUrls = 'showChangeLogsUrls';
+
+final START_VERSION = new RegExp(r'^' // Start at beginning.
+    r'(\d+).(\d+).(\d+)' // Version number.
+    r'(-([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?' // Pre-release.
+    r'(\+([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?'); // Build.
+
 main(List<String> arguments) async {
   exitCode = 0;
 
-  var parser = new ArgParser();
+  var parser = new ArgParser()
+    ..addFlag(showChangeLogsUrls, negatable: false, abbr: 'c');
   var parseResults = parser.parse(arguments);
 
   PubClient client = new PubClient();
@@ -18,6 +26,8 @@ main(List<String> arguments) async {
         "You need to provide a filename as argument (dart SpecCheck.dart file.yaml)");
     exit(-1);
   }
+
+  bool showChangeLogs = parseResults[showChangeLogsUrls];
 
   print("Processing this file: " + parseResults.rest[0]);
 
@@ -29,14 +39,28 @@ main(List<String> arguments) async {
     doc.dependencies.forEach((package, version) {
       if (package is String) {
         client.getPackage(package).then((onlinePackage) {
-          var currentConstraint =
+          sem.VersionRange currentConstraint =
               sem.VersionConstraint.parse(version.toString().split(" ")[1]);
           var newestVersion = sem.Version.parse(onlinePackage.latest.version);
 
           if (!currentConstraint.allows(newestVersion)) {
             print("\n--------- MAJOR DIFFERENCE ---------");
             print("Changelog: " +
-                "https://pub.dartlang.org/packages/"+package+"#-changelog-tab-");
+                "https://pub.dartlang.org/packages/" +
+                package +
+                "#-changelog-tab-");
+          } else if (showChangeLogs) {
+            if (currentConstraint.min.toString() ==
+                onlinePackage.latest.version.toString()) {
+                  print("------------------------------------");
+              print("No new version, no changelog needed");
+            } else {
+              print("------------------------------------");
+              print("Changelog: " +
+                  "https://pub.dartlang.org/packages/" +
+                  package +
+                  "#-changelog-tab-");
+            }
           }
           print(version.toString() +
               " => " +
